44a9162c81ade99538a722b3e3f4e8323b5d68f4
CataBasso <cbasso@fi.uba.ar>
2025-09-15 18:22:44 +0000

    Interfaz de linea de comandos de upload.

--- src/upload.py
+++ src/upload.py
@@ -1,27 +1,44 @@
 import socket
 import sys
+import argparse
 
-if len(sys.argv) != 3:
-    print("Usage: python3 upload.py <host> <port>")
-    sys.exit(1)
+def main():
+    parser = argparse.ArgumentParser(
+        description="<comand description>"
+    )
+    verbosity = parser.add_mutually_exclusive_group()
+    verbosity.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+    verbosity.add_argument("-q", "--quiet", action="store_true", help="decrease output verbosity")
+    parser.add_argument("-H", "--host", metavar="", help="server IP address")
+    parser.add_argument("-p", "--port", type=int, metavar="", help="server port")
+    parser.add_argument("-s", "--src", metavar="", help="source file path")
+    parser.add_argument("-n", "--name", metavar="", help="file name")
+    parser.add_argument("-r", "--protocol", metavar="", help="error recovery protocol")
 
-host = sys.argv[1]
-port = int(sys.argv[2])
+    parser._optionals.title = "optional arguments"
+    args = parser.parse_args()
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-client_socket.connect((host, port))
-print(f"Connected to server at {host}:{port}")
+    if not args.host or not args.port:
+        print("Usage: python3 upload.py -H <host> -p <port>")
+        sys.exit(1)
 
-while True:
-    message = input("Enter message to send (or 'exit' to quit): ")
-    client_socket.sendto(message.encode(), (host, port))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    client_socket.connect((args.host, args.port))
+    print(f"Connected to server at {args.host}:{args.port}")
 
-    if message == "exit":
-        print("Exiting client.")
-        break
+    while True:
+        message = input("Enter message to send (or 'exit' to quit): ")
+        client_socket.sendto(message.encode(), (args.host, args.port))
 
-    data, addr = client_socket.recvfrom(1024)
-    print(f"Received from server: {data.decode()}")
+        if message == "exit":
+            print("Exiting client.")
+            break
 
-client_socket.close()
+        data, addr = client_socket.recvfrom(1024)
+        print(f"Received from server: {data.decode()}")
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
