ae666cee271ac2c96355e5d51e475df90dd94851
CataBasso <cbasso@fi.uba.ar>
2025-09-15 18:52:24 +0000

    Se aplica lo mismo que esta implementado para upload.py pero para download.py (aun no pueden estar las dos aplicaciones conectadas a la vez).

---
+++ src/download.py
@@ -0,0 +1,50 @@
+import socket
+import sys
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="<comand description>",
+        usage="download [-h] [-v | -q] [-H ADDR] [-p PORT] [-d FILEPATH] [-n FILENAME] [-r protocol]"
+    )
+    verbosity = parser.add_mutually_exclusive_group()
+    verbosity.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+    verbosity.add_argument("-q", "--quiet", action="store_true", help="decrease output verbosity")
+    parser.add_argument("-H", "--host", metavar="", help="server IP address")
+    parser.add_argument("-p", "--port", type=int, metavar="", help="server port")
+    parser.add_argument("-d", "--dst", metavar="", help="destination file path")
+    parser.add_argument("-n", "--name", metavar="", help="file name")
+    parser.add_argument("-r", "--protocol", metavar="", help="error recovery protocol")
+
+    parser._optionals.title = "optional arguments"
+    args = parser.parse_args()
+
+    # esto en realidad seria:
+    # if not args.host or not args.port or not args.dst or not args.name:
+    #   print("Usage: python3 download.py -H <host> -p <port> -d <destination> -n <name>")
+    #   sys.exit(1)
+    # pero por ahora lo dejamos asi para probar la comunicacion
+    # habria que decidir si, si o si tenes que ponerle un destination path o si hay un path por defecto onda '/descargas'
+    if not args.host or not args.port:
+        print("Usage: python3 download.py -H <host> -p <port>")
+        sys.exit(1)
+
+    download_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    download_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print(f"Connected to server at {args.host}:{args.port}")
+
+    while True:
+        message = input("Enter message to send (or 'exit' to quit): ")
+        download_socket.sendto(message.encode(), (args.host, args.port))
+
+        if message == "exit":
+            print("Exiting client.")
+            break
+
+        data = download_socket.recvfrom(1024)
+        print(f"Received from server: {data.decode()}")
+
+    download_socket.close()
+
+if __name__ == "__main__":
+    main()
