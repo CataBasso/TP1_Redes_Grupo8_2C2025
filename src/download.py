5073ca8d67ed76bd250b93af241674404f7d35ac
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2025-09-23 20:04:20 -0300

    refactor: implement logging for error handling and status updates in download, upload, and server scripts

--- src/download.py
+++ src/download.py
@@ -1,14 +1,23 @@
 import os
 import sys
 import time
+import logging
 from lib.download_protocol import DownloadProtocol
 from lib.parser import get_parser
 
 def main():
     args = get_parser("download")
-    
+       
+    level = logging.INFO
+    if args.verbose:
+        level = logging.DEBUG
+    elif args.quiet:
+        level = logging.ERROR
+   
+    logging.basicConfig(level=level, format='%(asctime)s - %(levelname)s - %(message)s')
+
     if not args.host or not args.port or not args.dst or not args.name:
-        print("Usage: python3 download.py -H <host> -p <port> -d <destination> -n <name>")
+        logging.error("Usage: python3 download.py -H <host> -p <port> -d <destination> -n <name>")
         sys.exit(1)
 
     protocol = DownloadProtocol(args)
@@ -18,11 +27,11 @@ def main():
 
     if download:
         duration = end_time - start_time
-        
-        print(f"\n--- Transferencia Completa ---")
-        print(f"Tiempo total: {duration:.4f} segundos.")
+
+        logging.info(f"\n--- Transferencia Completa ---")
+        logging.info(f"Tiempo total: {duration:.4f} segundos.")
     else:
-        print("\nLa transferencia de archivos falló.")
+        logging.error("\nLa transferencia de archivos falló.")
         sys.exit(1)
 
 
