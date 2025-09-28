e6866db2311ea8ab3af2d7ebd9f979968436a537
Igris-1 <marcelo_n97@outlook.es>
2025-09-28 20:13:07 -0300

    Remove unused makefile and script for running server and clients

--- makefile
+++
@@ -1,4 +0,0 @@
-run:
-	gnome-terminal -- bash -c "python3 src/start-server.py -H 127.0.0.1 -p 5000; exec bash" &
-	gnome-terminal -- bash -c "python3 src/download.py -H 127.0.0.1 -p 5000; exec bash" &
-	gnome-terminal -- bash -c "python3 src/upload.py -H 127.0.0.1 -p 5000; exec bash" &
--- run_tmux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tmux new-session -d -s tp1 'python3 src/start-server.py -H 127.0.0.1 -p 5000'
-tmux split-window -v -t tp1 'python3 src/download.py -H 127.0.0.1 -p 5000'
-tmux split-window -h -t tp1:0.1 'python3 src/upload.py -H 127.0.0.1 -p 5000'
-tmux select-layout -t tp1 tiled
-tmux attach -t tp1
